e470f2166fd465ec8eac9c116a9bf8f45665aff8
Kayada09 <127507359+Kayada09@users.noreply.github.com>
2024-05-02 22:29:06 +0800

    Add files via upload

---
+++ Posttest6/Pt6/src/Tenda.java
@@ -0,0 +1,25 @@
+public class Tenda extends Camping {
+    private int kapasitas;
+
+    public Tenda(String kode, String nama, int harga, int kapasitas) {
+        super(kode, nama, harga);
+        this.kapasitas = kapasitas;
+    }
+
+    public int getKapasitas() {
+        return kapasitas;
+    }
+
+    public void setKapasitas(int kapasitas) {
+        this.kapasitas = kapasitas;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Tenda - Kode: " + kode + ", Nama: " + nama + ", Harga: " + harga + ", Kapasitas: " + kapasitas);
+    }
+
+    public final void display(int index) {
+        System.out.println("Index: " + index + ", Kode: " + kode + ", Nama: " + nama + ", Harga: " + harga + ", Kapasitas: " + kapasitas);
+    }
+}
